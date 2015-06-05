be2c1e41e4666873553510422a811ffbb61f9192
Sebastien Cheraux <scheraux@gmail.com>
2015-06-04 17:27:35 -0700

    Update models for following

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,3 +1,7 @@
 class Connection < ActiveRecord::Base
-  # Remember to create a migration!
+  validates :leader, presence:true
+  validates :follower, presence:true
+
+  belongs_to :leader, foreign_key: :leader_id, class_name: 'Lover'
+  belongs_to :follower, foreign_key: :follower_id, class_name: 'Lover'
 end
--- app/models/lover.rb
+++ app/models/lover.rb
@@ -6,6 +6,12 @@ class Lover < ActiveRecord::Base
   validates :alias, :email, uniqueness: true, presence: true
   validates :password_hash, presence: true
 
+  has_many :follower_connections, foreign_key: :follower_id, class_name: "Connection", dependent: :destroy
+  has_many :leaders, through: :follower_connections, source: :leader
+
+  has_many :leader_connections, foreign_key: :leader_id, class_name: "Connection", dependent: :destroy
+  has_many :followers, through: :leader_connections, source: :follower
+
   has_many :like_updates, dependent: :destroy
   has_many :liked_artworks, through: :like_updates, source: :artwork
 
